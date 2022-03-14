6e7e3729df81bc286563c7a67f57ff78740440dd
youknowjuno <samuel.hyun.j@gmail.com>
2022-03-13 18:01:12 -0700

    initial commit

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,25 @@
+import "./menu.scss";
+
+export default function menu() {
+  return (
+    <div className="menu">
+        <ul>
+            <li>
+                <a href="#intro">Home</a>
+            </li>
+            <li>
+                <a href="#intro">Home</a>
+            </li>
+            <li>
+                <a href="#intro">Home</a>
+            </li>
+            <li>
+                <a href="#intro">Home</a>
+            </li>
+            <li>
+                <a href="#intro">Home</a>
+            </li>
+        </ul>
+    </div>
+  )
+}
